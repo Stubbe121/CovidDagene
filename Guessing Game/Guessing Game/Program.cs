cc8dbde34b4e82fa54b21a9c3b911ccb64eafebc
Stubbe121 <59559005+Stubbe121@users.noreply.github.com>
2020-03-18 11:15:14 +0100

    Guessing game
    Begyndte på guessing game fordi havde brug for pause fra lommeregener

---
+++ Guessing Game/Guessing Game/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guess;
+            bool run = false;
+            Random n = new Random();
+            int number = n.Next(1,10);
+
+            while (!run)
+            {
+                try
+                {
+                    Console.WriteLine("Guess the number: ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+
+                    if (guess == number)
+                    {
+                        Console.WriteLine("Congrats you guessed it!!!");
+                    }
+                    else if (guess != number)
+                    {
+                        Console.WriteLine("Wrong number try again");
+                    }
+                }
+            }
+        }
+    }
+}
