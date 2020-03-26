631b4267ca4d18cfe05f3a21f6788cef26737181
Stubbe121 <59559005+Stubbe121@users.noreply.github.com>
2020-03-26 14:50:14 +0100

    HTMl og OOP begyndelse

---
+++ OOP Space Invaders 2.0/OOP Space Invaders 2.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Space_Invaders_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
