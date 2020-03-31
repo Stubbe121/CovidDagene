efd8b1ead5f8ee025527ef277ab57380c3bcf805
Stubbe121 <59559005+Stubbe121@users.noreply.github.com>
2020-03-31 15:49:25 +0200

    Pizza!!!

--- Pizzaria Uge 14 projekt/Pizzaria Uge 14 projekt/Byg_Selv_Pizza.cs
+++ Pizzaria Uge 14 projekt/Pizzaria Uge 14 projekt/Byg_Selv_Pizza.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace Pizzaria_Uge_14_projekt
 {
-    public partial class Form1 : Form
+    public partial class Byg_Selv_Pizza : Form
     {
-        public Form1()
+        public Byg_Selv_Pizza()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
